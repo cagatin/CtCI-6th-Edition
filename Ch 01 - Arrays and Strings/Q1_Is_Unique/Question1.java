be7f8436ff7be5fbea8ca41ed75f05309f313c3c
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-28 09:19:11 -0700

    Added solution for question 1

---
+++ Ch 01 - Arrays and Strings/Q1_Is_Unique/Question1.java
@@ -0,0 +1,23 @@
+import java.util.Set;
+import java.util.HashSet;;
+
+/* Task: Implement an algorithm to determine if a string has all unique characters*/
+public class Question1 {
+    public static boolean isUnique(String str) {
+        Set<Character> set = new HashSet<Character>();
+        for (int i = 0; i < str.length(); i++) {
+            if (set.contains(str.charAt(i))) {
+                return false;
+            }
+            set.add(str.charAt(i));
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[] { "Hello", "World", "Night", "Kitten" };
+        for (String word : words) {
+            System.out.println(word + ":" + isUnique(word));
+        }
+    }
+}
