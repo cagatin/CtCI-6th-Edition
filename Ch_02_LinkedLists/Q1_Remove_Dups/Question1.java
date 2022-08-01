79e32a8162e2871fce8b00858c496089183a6640
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-01 14:59:30 -0700

    Created started code for removeDups

--- Ch_02_LinkedLists/Q1_Remove_Dups/Question1.java
+++ Ch_02_LinkedLists/Q1_Remove_Dups/Question1.java
@@ -1,9 +1,15 @@
 package Ch_02_LinkedLists.Q1_Remove_Dups;
 
-import Ch_02_LinkedLists.LinkedList;
+import Ch_02_LinkedLists.ListNode;
 
 public class Question1 {
-    public void removeDups() {
+    public void removeDups(ListNode head) {
+        ListNode curr =  
+    }
 
+    public static void main(String[] args) {
+        ListNode L = new ListNode(1);
+        L.next = new ListNode(1);
+        L.next = new ListNode(1);
     }
 }
