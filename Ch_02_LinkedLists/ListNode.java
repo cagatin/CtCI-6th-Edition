bfcf2daa449d2c23c7d3dc2b7c3ceb19e9c5b857
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-01 14:54:18 -0700

    Created listnode class

---
+++ Ch_02_LinkedLists/ListNode.java
@@ -0,0 +1,20 @@
+package Ch_02_LinkedLists;
+
+// Definition code taken from Leetcode.com
+
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
---
+++ Ch_02_LinkedLists/Q1_Remove_Dups/Question1.java
@@ -0,0 +1,9 @@
+package Ch_02_LinkedLists.Q1_Remove_Dups;
+
+import Ch_02_LinkedLists.LinkedList;
+
+public class Question1 {
+    public void removeDups() {
+
+    }
+}
