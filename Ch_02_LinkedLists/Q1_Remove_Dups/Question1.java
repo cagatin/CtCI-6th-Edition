ba8e79dc51aac525beb72eb81941b9601ed1507d
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-02 11:42:37 -0700

    Added my solution for removeDups

--- Ch_02_LinkedLists/Q1_Remove_Dups/Question1.java
+++ Ch_02_LinkedLists/Q1_Remove_Dups/Question1.java
@@ -3,13 +3,22 @@ package Ch_02_LinkedLists.Q1_Remove_Dups;
 import Ch_02_LinkedLists.ListNode;
 
 public class Question1 {
-    public void removeDups(ListNode head) {
-        ListNode curr =  
+    public ListNode removeDups(ListNode head) {
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
     }
 
     public static void main(String[] args) {
         ListNode L = new ListNode(1);
         L.next = new ListNode(1);
         L.next = new ListNode(1);
+
     }
 }
